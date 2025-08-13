2967d6a0b8226cef62072e0caad3aa31eb8bf5a9
John Arthorne <john.arthorne@gmail.com>
2025-08-13 11:48:45 -0400

    Initial files

---
+++ tests/test_solver.py
@@ -0,0 +1,39 @@
+import unittest
+from src.solver import SudokuSolver
+
+class TestSudokuSolver(unittest.TestCase):
+
+    def test_solve_easy_puzzle(self):
+        puzzle = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+        self.solver = SudokuSolver(puzzle)
+        self.solver.solve()
+        self.assertTrue(self.solver.is_solved())
+
+    def test_unsolvable_puzzle(self):
+        puzzle = [
+            [5, 5, 0, 0, 7, 0, 0, 0, 0],  # Invalid: two 5's in the first row
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+        self.solver = SudokuSolver(puzzle)
+        self.solver.solve()
+        self.assertFalse(self.solver.is_solved())
+
+if __name__ == '__main__':
+    unittest.main()
